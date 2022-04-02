6e4f5bd703e0cb70ea8e70c290ab1d88354c6710
Nazim Uddin Nakib <46005032+NakibCSE@users.noreply.github.com>
2022-04-02 16:17:30 +0600

    Time: 1 ms (84.59%), Space: 54.5 MB (63.34%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,13 +1,27 @@
 class Solution {
     public void reverseString(char[] s) {
+        // Calculate the string length
         int len = s.length;
+        
+        // Initialize two pointers
         int i = 0, j = len-1;
         
+        //Loop though the string till two pointers meet
         while(i<=j){
+            
+            // Sotre i'th charater to a temporary variable called temp
             char temp = s[i];
+            
+            //Copy j'th character to i'th position 
             s[i] = s[j];
+            
+            //Copy the character stored in temp variable to j'th position
             s[j] = temp;
+            
+            //Increase the first pointer by 1
             i++;
+            
+            //Decrease the second pointer by 1
             j--;
         }
     }
