acb7d6a1f9624477fe35d27413b602099c028b9d
Md. Nazim Uddin <46005032+NakibCSE@users.noreply.github.com>
2022-04-05 10:52:20 +0600

    Time: 0 ms (100.00%), Space: 43.3 MB (67.07%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,23 +1,33 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        // Initialize lower bound and upper bound
         int low = 0, high = nums.length - 1;
         
+        //Loop through till low is less than or equal to high
         while(low<=high)
         {
+            //Calculate mid
             int mid = (low+high)/2;
+            
+            //nums[mid] is equal to target, return mid
             if(nums[mid] == target)
             {
                 return mid;
             }
+            
+            //If traget is greatter than nums[mid], go right
             else if(target> nums[mid]){
                 low = mid+1;
             }
+            //If Target is less than nums[mid], go left
             else
             {
                 high = mid-1;
             }
            
         }
+        
+        //Return low (in which position target needed to be insertted)
         return low;
     }
 }
